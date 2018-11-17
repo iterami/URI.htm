8541f96b92b148400e63725aada68dcee2f4fa8b
Jan Hořava <czechinthemail@gmail.com>
2018-11-17 11:59:49 -0800

    removed canvas

--- js/main.js
+++ js/main.js
@@ -1,31 +1,5 @@
 'use strict';
 
-function draw_logic(){
-    try{
-        canvas_buffer.drawImage(
-          image,
-          canvas_properties['width-half'] + camera_x,
-          canvas_properties['height-half'] + camera_y
-        );
-    }catch(error){
-    }
-}
-
-function logic(){
-    if(core_keys[core_storage_data['move-←']]['state']){
-        camera_x += core_storage_data['camera-speed'];
-    }
-    if(core_keys[core_storage_data['move-→']]['state']){
-        camera_x -= core_storage_data['camera-speed'];
-    }
-    if(core_keys[core_storage_data['move-↓']]['state']){
-        camera_y -= core_storage_data['camera-speed'];
-    }
-    if(core_keys[core_storage_data['move-↑']]['state']){
-        camera_y += core_storage_data['camera-speed'];
-    }
-}
-
 function repo_init(){
     core_repo_init({
       'events': {
@@ -44,32 +18,18 @@ function repo_init(){
               window.open(document.getElementById('uri').value);
           },
         },
-        'view': {
-          'onclick': function(){
-              canvas_setmode();
-
-              camera_x = 0;
-              camera_y = 0;
-              image.src = core_storage_data['uri'];
-
-              core_escape();
-          },
-        },
       },
       'globals': {
         'camera_x': 0,
         'camera_y': 0,
         'image': new Image,
       },
-      'info': '<textarea id=uri></textarea><br><input id=view type=button value=View><input id=open type=button value="Open URI in New Tab"><hr>'
+      'info': '<textarea id=uri></textarea><br><input id=open type=button value="Open URI in New Tab"><hr>'
         + '<input id=file type=file><input id=file-to-uri type=button value="Convert File to URI">',
       'menu': true,
       'storage': {
-        'camera-speed': 5,
         'uri': 'data:,',
       },
-      'storage-menu': '<table><tr><td><input id=camera-speed><td>Camera Speed</table>',
       'title': 'URI.htm',
     });
-    canvas_init();
 }
