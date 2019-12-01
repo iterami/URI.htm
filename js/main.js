f5c69a6335b990e1636aeabe48880457ade85aa5
Jan Hořava <czechinthemail@gmail.com>
2019-12-01 13:47:30 -0800

    added basic URI parse using new URL()

--- js/main.js
+++ js/main.js
@@ -23,8 +23,38 @@ function repo_init(){
               window.open(document.getElementById('uri').value);
           },
         },
+        'parse': {
+          'onclick': function(){
+              let result = '';
+              let uri = new URL(document.getElementById('uri').value);
+
+              let components = {
+                'hash': uri['hash'],
+                'host': uri['host'],
+                'origin': uri['origin'],
+                'pathname': '<textarea>' + uri['pathname'] + '</textarea>',
+                'port': uri['port'],
+                'protocol': uri['protocol'],
+                'search': uri['search'],
+              };
+
+              for(let component in components){
+                  result += '<tr><td>' + component + '<td>' + components[component];
+              }
+
+              document.getElementById('parsed').innerHTML = result;
+          },
+        },
+        'parse-clear': {
+          'onclick': function(){
+              document.getElementById('parsed').innerHTML = '';
+          },
+        },
       },
-      'info': '<textarea id=uri></textarea><br><input id=open type=button value="Open URI in New Tab"><hr>'
+      'info': '<textarea id=uri></textarea><br>'
+        + '<input id=open type=button value="Open URI in New Tab">'
+        + '<input id=parse type=button value="Parse URI"><input id=parse-clear type=button value="Clear Parse">'
+        + '<table id=parsed></table><hr>'
         + '<input id=file type=file><input id=file-to-uri type=button value="Convert File to URI">',
       'menu': true,
       'storage': {
