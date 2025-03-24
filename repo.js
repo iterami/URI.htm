a0cc72e740b404e97a576df7798593f09daf894f
Jan Hořava <czechinthemail@gmail.com>
2025-03-24 05:18:39 -0700

    replaced <hr> with better spacing

--- repo.js
+++ repo.js
@@ -58,7 +58,7 @@ function repo_init(){
       'info': '<textarea id=uri></textarea><br>'
         + '<button id=open type=button>Open URI</button>'
         + '<button id=parse type=button>Toggle Parse</button>'
-        + '<table id=parsed></table><hr>'
+        + '<table id=parsed></table>'
         + '<input id=file type=file><button id=file-to-uri type=button>Convert File to URI</button>',
       'menu-lock': true,
       'storage': {
